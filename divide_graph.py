c6d90a6bc3b511a74292942ff5361d93317371e2
zzyyds <zzyyds@umich.edu>
2023-04-10 16:54:20 -0400

    change start addr

--- divide_graph.py
+++ divide_graph.py
@@ -19,10 +19,10 @@ node_pointers=[int(line) for line in lines[3:total_nodes+3]]
 edge_list = lines[total_nodes+3:]
 # Calculate the number of nodes in each file
 nodes_per_file = total_nodes // 256 +1
-print("nodes_per_file",nodes_per_file)
+# print("nodes_per_file",nodes_per_file)
 # remainder_nodes = total_nodes % 256
 remainder_nodes = total_nodes-(nodes_per_file)*255
-print("remainder_nodes",remainder_nodes)
+# print("remainder_nodes",remainder_nodes)
 start_node=0
 
 for i in range(256):
@@ -47,7 +47,7 @@ for i in range(256):
             output_file.write(f"{num_nodes}\n{num_edges}\n")
             for i in range(num_nodes + 1):
                 edge_start = start_pointer
-                output_file.write(f"{node_pointers[start_node + i] - node_pointers[start_node]}\n")  # -node_pointers[start_node]
+                output_file.write(f"{node_pointers[start_node + i] - node_pointers[start_node]+ num_nodes*2}\n")  # -node_pointers[start_node]
             # for i in range(num_nodes + 1):
             #     output_file.write(f"{edge_list[node_pointers[start_node+i]:node_pointers[start_node+i+1]]}")
             for i in range(num_nodes + 1):
@@ -56,11 +56,11 @@ for i in range(256):
                     output_file.write(f"{edge}")
             # for j in range(num_nodes):
     else:
-        print("accessing last vault")
+        # print("accessing last vault")
         end_node = start_node + remainder_nodes-1
-        print("start_node:", start_node)
-        print("remaining nodes:",remainder_nodes)
-        print("end_node",end_node)
+        # print("start_node:", start_node)
+        # print("remaining nodes:",remainder_nodes)
+        # print("end_node",end_node)
         num_nodes = remainder_nodes
 
         start_pointer = node_pointers[start_node]
@@ -74,7 +74,7 @@ for i in range(256):
             output_file.write(f"{num_nodes}\n{num_edges}\n")
             for i in range(num_nodes):
                 edge_start = start_pointer
-                output_file.write(f"{node_pointers[start_node + i] - node_pointers[start_node]}\n")  # -node_pointers[start_node]
+                output_file.write(f"{node_pointers[start_node + i] - node_pointers[start_node] + num_nodes*2 }\n")  # -node_pointers[start_node]
                 # output_file.write(f"{edge_list[start_pointer:]}")
             # for i in range(num_nodes-1):
             #     output_file.write(f"{edge_list[node_pointers[start_node+i]:node_pointers[start_node+i+1]]}")
@@ -98,8 +98,8 @@ for i in range(256):
     #         output_file.write(f"{node_pointers[start_node+i]}\n")#-node_pointers[start_node]
     #         #output_file.write(f"{edge_list[start_pointer:]}")
     #     # for j in range(num_nodes):
-    print("start_node",start_node)
-    print("end_node", end_node)
+    # print("start_node",start_node)
+    # print("end_node", end_node)
     start_node = end_node+1
 # for i in range(256):
 #     for i in range(num_nodes):
