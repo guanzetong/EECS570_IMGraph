870e543e122e150da69e0a49c71c3c19c54f9c6a
Stzzzzy <stzzzy@umich.edu>
2023-04-13 18:52:46 -0400

    eq_modified

--- test_eq_feed.py
+++ test_eq_feed.py
@@ -1,6 +1,7 @@
 from event_queues import EQ
 from collections import deque
-
+from standard import mem_response
+import numpy as np
 
 ep_e_i = deque()
 ep_e_o = deque()
@@ -16,8 +17,9 @@ num_vaults=32
 
 eq = EQ(ep_e_i, ep_e_o, ep_n_i, ep_n_o, ep_w_i, ep_w_o, ep_s_i, ep_s_o, ep_idx_ranges, num_vaults, func = "pagerank")
 
-for i in range(500):
-    print(f"------------------ cycle: {i} -----------------")
+for i in range(800000):
+    print(f"----------------------------- cycle: {i} -----------------------------")
     # if i == 10:
-    #     eq.vault_mem[0].response_port.append([1 for i in range(256)])
-    eq.one_cycle()
+        #  eq.vault_mem[0].response_port.append(mem_response([0.15 for i in range(256)],1))
+    eq.one_clock_try()
+    
